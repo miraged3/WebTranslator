f166d39c62ae741a2629c4d18e0bafc4ed287b00
Maooookai <1220568032@qq.com>
2022-08-18 11:36:36 +0800

    bug fix

--- caiyun.py
+++ caiyun.py
@@ -3,16 +3,21 @@ import random
 import time
 
 from selenium import webdriver
+from selenium.webdriver import ActionChains
+from selenium.webdriver.common.by import By
 
 print("彩云翻译工具")
-print("by Mirage 2021.5.19")
+print("by Mirage 2022.8.17")
 print("使用前请确认待翻译文件trans.txt已放在本目录")
 print("正在启动chromedriver...")
 browser = webdriver.Chrome(r'./chromedriver')
 browser.get('https://fanyi.caiyunapp.com/')
 print('等待网页加载...')
 time.sleep(3)
-inputArea = browser.find_element_by_class_name('textinput')
+action = ActionChains(browser)
+action.move_by_offset(100, 100).click().perform()
+time.sleep(3)
+inputArea = browser.find_element(By.CLASS_NAME, 'textinput')
 fileName = 'Translate-' + time.strftime("%Y-%m-%d_%H.%M.%S", time.localtime()) + '.txt'
 result = open(fileName, 'w', encoding='utf-8')
 current = 0
@@ -24,7 +29,7 @@ else:
 target = ''
 lines = 0
 print('正在加载进度...')
-for line in open('trans.txt', encoding='utf-8', errors='ignore'):
+for line in open('trans1.txt', encoding='utf-8', errors='ignore'):
     while i <= current:
         i += 1
         break
@@ -39,7 +44,7 @@ for line in open('trans.txt', encoding='utf-8', errors='ignore'):
             time.sleep(random.randint(600, 1000) / 100)
             for index in range(lines):
                 xpath = '//div[@id=\'texttarget\']/p[' + str(index + 1) + ']/span'
-                text = browser.find_element_by_xpath(xpath)
+                text = browser.find_element(By.XPATH, xpath)
                 print(text.text)
                 result.write(text.text)
                 result.write('\n')
@@ -62,7 +67,7 @@ print('行数：' + str(lines))
 time.sleep(random.randint(600, 1000) / 100)
 for index in range(lines):
     xpath = '//div[@id=\'texttarget\']/p[' + str(index + 1) + ']/span'
-    text = browser.find_element_by_xpath(xpath)
+    text = browser.find_element(By.XPATH, xpath)
     print(text.text)
     result.write(text.text)
     result.write('\n')
@@ -73,6 +78,5 @@ open('log', 'r+', encoding='utf-8', errors='ignore').truncate()
 open('log', 'r+', encoding='utf-8', errors='ignore').write(str(current))
 lines = 0
 result.flush()
-
 browser.quit()
 browser.stop_client()
