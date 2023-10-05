0a15809821846729d2fda4259aad39ddb9948b63
miraged3 <1220568032@qq.com>
2023-10-05 16:27:35 +0800

    bug fix

--- caiyun.py
+++ caiyun.py
@@ -3,21 +3,22 @@ import random
 import time
 
 from selenium import webdriver
+from selenium.webdriver.common.by import By
 
 print("彩云翻译工具")
 print("by Mirage 2021.5.19")
 print("使用前请确认待翻译文件trans.txt已放在本目录")
 print("正在启动chromedriver...")
-browser = webdriver.Chrome(r'chromedriver.exe')
+browser = webdriver.Chrome()
 browser.get('https://fanyi.caiyunapp.com/')
 print('等待网页加载...')
 time.sleep(3)
-inputArea = browser.find_element_by_class_name('textinput')
+inputArea = browser.find_element(By.CLASS_NAME, 'textinput')
 fileName = 'Translate-' + time.strftime("%Y-%m-%d_%H.%M.%S", time.localtime()) + '.txt'
 result = open(fileName, 'w', encoding='utf-8')
 current = 0
 i = 0
-if os.path.exists('log'):
+if os.path.exists('log') and os.path.getsize('log') != 0:
     current = int(open('log', encoding='utf-8', errors='ignore').read())
 else:
     open('log', 'w', encoding='utf-8')
@@ -29,7 +30,7 @@ for line in open('trans.txt', encoding='utf-8', errors='ignore'):
         i += 1
         break
     if i > current:
-        if len(target) + len(line) >= 4500:
+        if len(target) + len(line) >= 4000:
             target = target + line
             print(line)
             lines += 1
@@ -37,13 +38,10 @@ for line in open('trans.txt', encoding='utf-8', errors='ignore'):
             print('等待返回结果...')
             print('行数：' + str(lines))
             time.sleep(random.randint(600, 1000) / 100)
-            for index in range(lines):
-                xpath = '//div[@id=\'texttarget\']/p[' + str(index + 1) + ']/span'
-                text = browser.find_element_by_xpath(xpath)
-                print(text.text)
-                result.write(text.text)
-                result.write('\n')
-
+            text_target = browser.find_element(By.XPATH, '//*[@id="texttarget"]')
+            print(text_target.text)
+            result.write(text_target.text)
+            result.write('\n')
             inputArea.clear()
             target = ''
             current = current + lines
